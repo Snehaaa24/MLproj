daaa6720e76b2b4a328531864735b6fdc29bb2be
Sneha Nair <150941178+Snehaaa24@users.noreply.github.com>
2025-01-20 21:44:15 +0530

    few errors removed  and functionality changed.

--- src/exception.py
+++ src/exception.py
@@ -1,12 +1,11 @@
-import logging
 import sys
-import logger
+import logging
 
 def error_message_details(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     filename=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in python script name [{0}] line number [{1}] error message [{2}]".format()
-    filename,exc_tb.tb_lineno,str(error)
+    error_message="Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+    filename,exc_tb.tb_lineno,str(error))
 
     return error_message    
 
@@ -17,4 +16,9 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("divided by zero")
+        raise CustomException(e,sys)
